216c4b52b62b6d916038747583b43c418e4e5f15
matteo <matteo.borghesi92@gmail.com>
2023-03-28 15:01:16 +0200

    fix

--- src/rw/scan_handler.py
+++ src/rw/scan_handler.py
@@ -15,7 +15,8 @@ class ScanWriter:
         some info here
         
         '''
-        self._file=h5py.File(output_path+'/'+name_file  +'.scan', "w")
+        self.__file = output_path+'/'+name_file  +'.scan'
+        #self._file=h5py.File(output_path+'/'+name_file  +'.scan', "w")
         
     def set_general_info(self, data='16/03', T_baw=0.2, N_baw=1, Note=None):
         
@@ -23,13 +24,14 @@ class ScanWriter:
         some info here
         
         '''
-        general_info=self._file.create_group('info')
+        with h5py.File(self.__file,'a') as f:
+            general_info=f.create_group('info')
         
         
-        general_info.create_dataset('date', data=str(data) )
-        general_info.create_dataset('temperature', data=T_baw )
-        general_info.create_dataset('baw_number', data=str(N_baw) )
-        general_info.create_dataset('note', data=str(Note) )
+            general_info.create_dataset('date', data=str(data) )
+            general_info.create_dataset('temperature', data=T_baw )
+            general_info.create_dataset('baw_number', data=str(N_baw) )
+            general_info.create_dataset('note', data=str(Note) )
         
         return
     
@@ -52,49 +54,45 @@ class ScanWriter:
         some info here
         
         '''
+        with h5py.File(self.__file,'a') as f:
+            all_data=f.create_group('data')
         
-        all_data=self._file.create_group('data')
         
-        
-        path_files = os.listdir(path)
-        #controllare che i files siano tutti .dat
+            path_files = os.listdir(path)
+            #controllare che i files siano tutti .dat
 
-        for i in range(len(path_files)):
-            if (path_files[i].find(file_name) != -1) & ((path_files[i].find(desin) != -1)):
-                file_path = path + '/'+ path_files[i]
-                
-                header = self.__read_header(file_path)
-                IFBW_riso=header['IFBW']
-                power_riso=header['power']
-                f_center = header['center']
+            for i in range(len(path_files)):
+                if (path_files[i].find(file_name) != -1) & ((path_files[i].find(desin) != -1)):
+                    file_path = path + '/'+ path_files[i]
+                    
+                    header = self.__read_header(file_path)
+                    IFBW_riso=header['IFBW']
+                    power_riso=header['power']
+                    f_center = header['center']
             
-                riso=all_data.create_group('resonance_'+str(i+1)) 
-                scan_riso=riso.create_group('scan_info')
-                scan_riso.create_dataset('ifbw',  data=IFBW_riso )
-                scan_riso.create_dataset('input_power', data=power_riso)
-                scan_riso.create_dataset('f_center', data=f_center)
-                
-                data = np.loadtxt(file_path)
-                data_riso=riso.create_group('data')  
-                
-                for k in range(len(data_pos)):
-                    data_riso.create_dataset(data_names[k], data=data[:,int(data_pos[k])]  )  
-            else:
-                pass
+                    riso=all_data.create_group('resonance_'+str(i+1)) 
+                    scan_riso=riso.create_group('parameters')
+                    scan_riso.create_dataset('ifbw',  data=IFBW_riso )
+                    scan_riso.create_dataset('input_power', data=power_riso)
+                    scan_riso.create_dataset('f_center', data=f_center)
+                    
+                    data = np.loadtxt(file_path)
+                    data_riso=riso.create_group('data')  
+                    
+                    for k in range(len(data_pos)):
+                        data_riso.create_dataset(data_names[k], data=data[:,int(data_pos[k])]  )  
+                else:
+                    pass
             
             
         return
-    
-    
-    def close_file(self):
         
-        self._file.close()
-        
+
+    def save_parameter(self,path,name,value):
+ 
+        with h5py.File(self.__file,'a') as f:       
+            f.create_dataset(path+'/'+name,data=value)
         return
-
-
-
-
 
 
 
@@ -102,8 +100,9 @@ class ScanWriter:
 class ScanReader:
     
     def __init__(self, path_file):
-        self._file=h5py.File(path_file, "r")
-        self._group_name = 'scan_info'
+        self.__file = path_file
+        #self._file=h5py.File(path_file, "r")
+        self._group_name = 'parameters'
 
     def _convert_to_dict(self, group):
         dic = {}
@@ -120,39 +119,44 @@ class ScanReader:
         return dic    
         
     def _group_tags(self,location):
-        return list(self._file['data'][list(self._file['data'].keys())[0]][location].keys())
+        with h5py.File(self.__file,'r') as f:
+            return list(f['data'][list(f['data'].keys())[0]][location].keys())
     
     def set_group_name(self,name):
         self._group_name = name
         return
     
     def get_scan_info(self):
-        return self._convert_to_dict(self._file['info'])
+        with h5py.File(self.__file,'r') as f:
+            return self._convert_to_dict(f['info'])
+            
+    def get_resonances_list(self):
+        with h5py.File(self.__file,'r') as f:
+            return list(f['data'].keys())
     
     def get_parameters_tags(self):
         return self._group_tags(self._group_name)
 
     def get_parameters(self,name=None):
-        temp = []
-        for el in self._file['data'].keys():
-            value = self._file['data'][el][self._group_name][name]
-            temp.append(np.array(value))
+        with h5py.File(self.__file,'r') as f:
+            temp = []
+            for el in f['data'].keys():
+                value = f['data'][el][self._group_name][name]
+                temp.append(np.array(value))
         return np.array(temp)
     
-    def get_resonance(self,number=None,freq=None,loc=None):
-        if freq is not None:
-            f = self.get_parameters('f_center')
-            resonance_name = list(self._file['data'].keys())[np.argmin(np.abs(f-freq))]
-        if number is not None:
-            resonance_name = 'resonance_'+str(number)
-        if loc is not None:
-            resonance_name = list(self._file['data'].keys())[loc]
+    def get_resonance(self,name=None,freq=None,loc=None):
+        with h5py.File(self.__file,'r') as f:
+            if freq is not None:
+                ff = self.get_parameters('f_center')
+                resonance_name = self.get_resonances_list()[np.argmin(np.abs(ff-freq))]
+            if name is not None:
+                resonance_name = name
+            if loc is not None:
+                resonance_name = self.get_resonances_list()[loc]
             
-        dic1 = self._convert_to_dict(self._file['data'][resonance_name][self._group_name])
-        dic2 = self._convert_to_dict(self._file['data'][resonance_name]['data'])
-        dic1.update(dic2)
+            dic1 = self._convert_to_dict(f['data'][resonance_name][self._group_name])
+            dic2 = self._convert_to_dict(f['data'][resonance_name]['data'])
+            dic1.update(dic2)
+            dic1['reso_name'] = resonance_name
         return dic1
-        
-    def close_file(self):
-        self._file.close()
-        return
