486e31a5ab6dc32f41218c92b683093e5f0b74e5
Riccardo Maifredi <77549304+Richardforphys@users.noreply.github.com>
2026-03-10 18:41:37 +0100

    Adjust sleep intervals and error handling in Execute.py

--- scripts/Execute.py
+++ scripts/Execute.py
@@ -3,31 +3,31 @@ from VPhi import fitter
 import subprocess
 import time
 
-INTERVAL = 10      # 10 seconds sleep time
-TOTAL_TIME = 6*3600   # 6 hours
+INTERVAL = 60      # 10 seconds sleep time
+TOTAL_TIME = 8*3600   # 6 hours
 dp = r"/home/cmsdaq/TexDPO3014/vphi_vs_time"
 start = time.time()
 
 tek = DPO3014()
+tek.write("HORizontal:SCAle 1E-2")
+tek.RL = 10000
+tek.set_average_mode()
+tek.write("ACQuire:NUMAVg 512")
+
 while (time.time() - start) < TOTAL_TIME:
-    tek.write("HORizontal:SCAle 1E-2")
-    tek.RL = 10000
-    tek.set_average_mode()
-    tek.write("ACQuire:NUMAVg 512")
     print("Acquiring data...")
-    while(True):
-        try:
-            result = tek.acquire()    
-            break
-        except:
-            print("Something went wrong with retrieval...trying again")
-            time.sleep(2)
+    tek.acquire(sleep=60)
     print("Fitting data...")
     fitfit = fitter(result["time"], result["CH1"], result["CH2"], dp)
-    fitfit.analyze_vphi(plot=False)
+    try:
+        fitfit.analyze_vphi(plot=False)
+    except:
+        print("Something went wrong with Vphi fit...continue")
+        pass
     print("zzz...")
     
     time.sleep(INTERVAL)
     
 print("Acquisition Ended!")
+print("Start time", start)
 del tek
